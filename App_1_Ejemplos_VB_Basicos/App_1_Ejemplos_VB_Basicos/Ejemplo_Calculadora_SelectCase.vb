b3f5c52c35e1dbebca7a83e3c8aa6934e91c881f
sinensia102 <sinensia102@VINET102>
2017-06-22 19:47:20 +0200

    Añadidos ejemplos Calcu_Select y Tabla_ForNext

---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_Calculadora_SelectCase.vb
@@ -0,0 +1,36 @@
+﻿Module Ejemplo_Calculadora_SelectCase
+
+    Sub MAIN()
+
+        Dim OPERANDO1 As Integer
+        Dim OPERANDO2 As Integer
+        Dim OPERADOR As Char
+        Dim RESULTADO As Single
+
+
+        Console.WriteLine("Introduce el primer operando: ")
+        OPERANDO1 = Console.ReadLine
+        Console.WriteLine("Introduce el OPERADOR: ")
+        OPERADOR = Console.ReadLine
+        Console.WriteLine("Introduce el segundo operando: ")
+        OPERANDO2 = Console.ReadLine
+
+        Select Case OPERADOR
+            Case "+"
+                RESULTADO = OPERANDO1 + OPERANDO2
+            Case "-"
+                RESULTADO = OPERANDO1 - OPERANDO2
+            Case "*"
+                RESULTADO = OPERANDO1 * OPERANDO2
+            Case "/"
+                RESULTADO = OPERANDO1 / OPERANDO2
+        End Select
+
+        Console.WriteLine("El resultado es " & RESULTADO)
+
+        Console.Read()
+
+
+    End Sub
+
+End Module
---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_Tabla_Multiplicar_ForNext.vb
@@ -0,0 +1,24 @@
+﻿Module Ejemplo_Tabla_Multiplicar_ForNext
+    Sub main()
+
+        Console.WriteLine("Introduce un número: ")
+        Dim número As Integer
+
+        For número = 1 To 10
+
+            número = Console.ReadLine
+            Dim i As Integer = 1
+            Do
+                Console.WriteLine(número & " x " & i & " = " & número * i)
+                i = i + 1
+            Loop Until i = 10
+
+        Next
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
