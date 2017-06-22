b34d6d236e4ff02482d6ca3c0b79a596124769e8
sinensia102 <sinensia102@VINET102>
2017-06-22 19:40:02 +0200

    Añadidos Ejemplos de Círculo y Arcotangente

---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_ArcoTangente.vb
@@ -0,0 +1,27 @@
+﻿Module Ejemplo_ArcoTangente
+
+    Sub main()
+        Dim x, y As Single
+        Dim Truncado, Redondeado As Integer
+
+        Console.WriteLine("Introduce el valor de x: ")
+        x = Console.ReadLine
+        Console.WriteLine("Introduce el valor de y: ")
+        y = Console.ReadLine
+
+        Console.WriteLine("Arcotan = " & Math.Atan2(y, x))
+
+        Truncado = Math.Floor(Math.Atan2(y, x))
+        Console.WriteLine("Arcotan Truncado = " & Truncado)
+        Redondeado = Math.Round(Math.Atan2(y, x))
+        Console.WriteLine("Arcotan Redondeado = " & Redondeado)
+
+
+
+        Console.Read()
+
+
+    End Sub
+
+
+End Module
---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_Circulo.vb
@@ -0,0 +1,33 @@
+﻿Module Ejemplo_Circulo
+
+    Sub main()
+
+        Dim Radio, area As Single
+        Dim Número_de_Círculos As Integer
+
+        Console.WriteLine("Introduce el número de círculos: ")
+        Número_de_Círculos = Console.ReadLine
+
+        For contador = 1 To Número_de_Círculos
+            Console.WriteLine("Introduce el radio en cm: ")
+            Radio = Console.ReadLine
+            area = Math.PI * Math.Pow(Radio, 2)
+            Console.WriteLine("Él área del círculo es: " & area)
+
+            Select Case area
+                Case < 10
+                    Console.WriteLine("El Círculo es pequeño")
+                Case 10 To 100
+                    Console.WriteLine("El Círculo es mediano")
+                Case Else
+                    Console.WriteLine("El Círculo es grande")
+            End Select
+        Next
+
+        Console.Read()
+
+    End Sub
+
+
+
+End Module
