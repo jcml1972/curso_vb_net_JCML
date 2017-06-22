b0de54719444bcdda14f93c3107233d5d41e5b0a
sinensia102 <sinensia102@VINET102>
2017-06-22 19:42:28 +0200

    Añadidos Ejemplos Tabla Multiplicar

---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_Tabla_Multiplicar.vb
@@ -0,0 +1,19 @@
+﻿Module Ejemplo_Tabla_Multiplicar
+
+    Sub main()
+
+        Console.WriteLine("Introduce un número: ")
+        Dim número As Integer = Console.ReadLine
+
+
+        For i As Integer = 1 To 10
+            Console.WriteLine(número & " x " & i & " = " & número * i)
+        Next
+
+        Console.Read()
+
+
+    End Sub
+
+
+End Module
---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_Tabla_Multiplicar_While.vb
@@ -0,0 +1,23 @@
+﻿Module Ejemplo_Tabla_Multiplicar_While
+
+    Sub main()
+
+        Console.WriteLine("Introduce un número: ")
+        Dim número As Integer = Console.ReadLine
+
+        While número > 0 And número < 10
+
+            For i As Integer = 1 To 10
+                Console.WriteLine(número & " x " & i & " = " & número * i)
+            Next
+            número = Console.ReadLine
+
+        End While
+        Console.ReadLine()
+
+
+    End Sub
+
+
+
+End Module
