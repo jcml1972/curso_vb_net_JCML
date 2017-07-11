5d577a940986a8f8698dfdc8946ea1392e17f2b9
Juan Carlos Martín Linares <juancar.linares@hotmail.com>
2017-07-11 18:43:02 +0200

    Añadido Try Catch en la exportación para controlar errores

--- App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosExcel.vb
+++ App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosExcel.vb
@@ -19,7 +19,11 @@
         End Property
 
         Public Function Importar(ByRef arrayEmpleados() As Empleado) As Boolean Implements IPersistenciaEmpleados.Importar
-            MessageBox.Show("Importar " & NombreFichero)
+
+
+
+
+            MessageBox.Show("Importado " & NombreFichero)
             'Throw New NotImplementedException()
         End Function
 
@@ -27,30 +31,35 @@
             Dim oExcel As Object
             Dim oLibro As Object
             Dim oHoja As Object
-            ' Creamos un objeto cuyo contenido es una instancia de Excel
-            oExcel = CreateObject("Excel.Application")
-            ' Comenzar nuevo libro de excel
-            oLibro = oExcel.Workbooks.Add
-            oHoja = oLibro.Worksheets(1)
+            Try
+                ' Creamos un objeto cuyo contenido es una instancia de Excel
+                oExcel = CreateObject("Excel.Application")
+                ' Comenzar nuevo libro de excel
+                oLibro = oExcel.Workbooks.Add
+                oHoja = oLibro.Worksheets(1)
 
-            Dim ArrayDatos(arrayEmpleados.Length - 1, 5)
-            Dim i As Integer
-            For i = 0 To arrayEmpleados.Length - 1
-                ArrayDatos(i, 0) = arrayEmpleados(i).nombre
-                ArrayDatos(i, 1) = arrayEmpleados(i).apellidos
-                ArrayDatos(i, 2) = arrayEmpleados(i).genero
-                ArrayDatos(i, 3) = arrayEmpleados(i).categoria
-                ArrayDatos(i, 4) = arrayEmpleados(i).retribucionFija
-            Next
-            oHoja.range("A1:E1").value = New String() {"NOMBRE", "APELLIDOS", "GÉNERO", "CATEGORÍA", "RETRIBUCIÓN FIJA"}
-            oHoja.Range("A2").resize(arrayEmpleados.Length - 1, 5).value = ArrayDatos
+                Dim ArrayDatos(arrayEmpleados.Length - 1, 5)
+                Dim i As Integer
+                For i = 0 To arrayEmpleados.Length - 1
+                    ArrayDatos(i, 0) = arrayEmpleados(i).nombre
+                    ArrayDatos(i, 1) = arrayEmpleados(i).apellidos
+                    ArrayDatos(i, 2) = arrayEmpleados(i).genero
+                    ArrayDatos(i, 3) = arrayEmpleados(i).categoria
+                    ArrayDatos(i, 4) = arrayEmpleados(i).retribucionFija
+                Next
+                oHoja.range("A1:E1").value = New String() {"NOMBRE", "APELLIDOS", "GÉNERO", "CATEGORÍA", "RETRIBUCIÓN FIJA"}
+                oHoja.Range("A2").resize(arrayEmpleados.Length, 5).value = ArrayDatos
 
-            ' Guardar el libro y salir de Excel
-            oLibro.saveas(NombreFichero)
-            oExcel.Quit()
+                ' Guardar el libro y salir de Excel
+                oLibro.saveas(NombreFichero)
+                oExcel.Quit()
 
-            MessageBox.Show("Exportado " & NombreFichero)
-            'Throw New NotImplementedException()
+                MessageBox.Show("Exportado " & NombreFichero)
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.ToString())
+                Return False
+            End Try
+            Return True
         End Function
 
     End Class
