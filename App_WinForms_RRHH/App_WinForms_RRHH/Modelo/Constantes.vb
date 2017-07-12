964878aff925d278417c0bfe51df79354bbd69c8
Juan Carlos Martín Linares <juancar.linares@hotmail.com>
2017-07-12 17:49:12 +0200

    Añadida Cadena de Conexión a Access

--- App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Constantes.vb
+++ App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Constantes.vb
@@ -6,6 +6,12 @@ Namespace Modelo
         Public Const CADENA_CONEX_EXCEL As String = "Provider=Microsoft.ACE.OLEDB.12.0;" &
                                                     "Extended Properties='Excel 12.0 Xml;HDR=No';" &
                                                     "Data Source="
+
+        Public Const CADENA_CONEX_ACCESS As String = "Provider=Microsoft.Jet.OLEDB.4.0;" &
+            "Data Source=" 'Justo aquí tiene que ir el nombre del fichero
+        ' Si la BD tuviera password habría que añadir, añadir separado por punto y coma, usuario y password
+        ' TODO: Añadir ";User Id=admin;Password=Contraseña_1234"
+
     End Module
     End Namespace
 
